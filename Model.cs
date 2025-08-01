ab6cb96d8266ba71694690cb218fc2a43c6e5663
Alberto Vargas Borsatto <albertoborsatto@hotmail.com>
2025-08-01 12:42:13 -0300

    fixing DbConnection

--- Model.cs
+++ Model.cs
@@ -1,32 +1,15 @@
 using LaboratoriosRestAPI.Models;
-
-namespace LaboratoriosRestAPI;
-
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 
-public class BookLendingContext : DbContext
+namespace LaboratoriosRestAPI
 {
-    // DbSet for Books
-    public DbSet<Livro> Livros { get; set; }
-    // DbSet for Users (who borrow books)
-    public DbSet<Autor> Autores { get; set; }
-    // DbSet for Loans (the act of a user borrowing a book)
-    public DbSet<Emprestimo> Emprestimos { get; set; }
-
-    public string DbPath { get; }
-
-    public BookLendingContext()
+    public class BookLendingContext : DbContext
     {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        // Changed the database file name to reflect the new domain
-        DbPath = System.IO.Path.Join(path, "booklending.db");
+        public BookLendingContext(DbContextOptions<BookLendingContext> options)
+            : base(options) { }
+
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Emprestimo> Emprestimos { get; set; }
     }
-
-    // Configures EF Core to use SQLite for the specified database file.
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
 }
-
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using LaboratoriosRestAPI;
+using LaboratoriosRestAPI.Controllers;
 using LaboratoriosRestAPI.Models;
 using LaboratoriosRestAPI.Repository;
 using LaboratoriosRestAPI.Repository.Implementations;
@@ -16,14 +17,18 @@ class Program
         var builder = WebApplication.CreateBuilder(args);
 
 // Registrar o contexto e serviços no container de DI
+
+        builder.Services.AddDbContext<BookLendingContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+        
         builder.Services.AddScoped<IAutorRepository, AutorRepository>();
         builder.Services.AddScoped<IAutorService, AutorService>();
 
         builder.Services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
         builder.Services.AddScoped<IEmprestimoService, EmprestimoService>();
         
-        builder.Services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
-        builder.Services.AddScoped<IEmprestimoService, EmprestimoService>();
+        builder.Services.AddScoped<ILivroService, LivroService>();
+        builder.Services.AddScoped<ILivroRepository, LivroRepository>();
 
 // Add suporte à API
         builder.Services.AddControllers();
